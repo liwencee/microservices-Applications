a4b4e409fedc9d06e9397bf1e1b8e3a18eafc9a0
Olalekan Akindiya <lehqandiya@gmail.com>
2025-08-12 11:37:48 +0100

    push to repo

--- eks-terraform/outputs.tf
+++ eks-terraform/outputs.tf
@@ -5,3 +5,11 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = module.eks_cluster.cluster_endpoint
 }
+
+
+output "eks_kubeconfig_command" {
+  description = "Command to configure kubectl for the EKS cluster"
+  value       = "aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name}"
+}
+
+
--- eks-terraform/terraform.tfvars
+++ eks-terraform/terraform.tfvars
@@ -1,2 +1,2 @@
-# region = "us-east-1"
-# cluster_name = "prod-eks"
+region = "us-east-1"
+cluster_name = "prod-eks"
